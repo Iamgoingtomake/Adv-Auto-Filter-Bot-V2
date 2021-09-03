424e07d045fc1c74fe1ce50ca57f246c198b5489
Iamgoingtomake <89531655+Iamgoingtomake@users.noreply.github.com>
2021-09-03 19:30:09 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -58,7 +58,7 @@ class Translation(object):
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ official Channel </b> : <i><a href="https://t.me/Asgardians_Official">Pyrogram Asyncio 1.13.0 </a></i>
 
 <b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
 """
